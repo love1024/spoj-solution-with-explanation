45c585d563e07301c0df1f6b71bf48a59acdb1ad
love1024 <lsk18mto@gmail.com>
2017-09-11 23:26:58 +0530

    Solution of CRSCNTRY

---
+++ CRSCNTRY.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//Longest common subsequence problem
+int main() {
+    int t;
+    cin>>t;
+
+    while(t--) {
+        vector<int> ages;
+        int c,mx=0;
+
+        cin>>c;
+        while(c != 0) {
+            ages.push_back(c);
+            cin>>c;
+        }
+
+        cin>>c;
+        while(c != 0) {
+            vector<int> tom;
+            do {
+                tom.push_back(c);
+                cin>>c;
+            }while(c != 0);
+
+            int arr[ages.size()+1][tom.size()+1];
+            memset(arr,0,sizeof(arr));
+
+            for(int i=0;i<ages.size();i++) {
+                for(int j=0;j<tom.size();j++) {
+                    if(ages[i] == tom[j])
+                        arr[i+1][j+1] = 1 + arr[i][j];
+                    else
+                        arr[i+1][j+1] = max(arr[i+1][j],arr[i][j+1]);
+                }
+            }
+            mx = max(mx,arr[ages.size()][tom.size()]);
+            cin>>c;
+        }
+
+        cout<<mx<<endl;
+    }
+}
