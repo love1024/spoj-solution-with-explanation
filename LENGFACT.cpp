304b18a31aae828c88f45ea74b07dd917d3626af
love1024 <lsk18mto@gmail.com>
2017-09-25 19:59:16 +0530

    Solution of LENGFACT

---
+++ LENGFACT.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// Use Kamenetsky formula to calculate the
+
+#include <iostream>
+#include <cmath>
+#define pi acos(-1.0)
+using namespace std;
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        double x; cin >> x;
+        if (x <= 1) cout << 1 << endl;
+        else cout << (long long)(1 + floor((0.5*log(2.0*pi*x) + x*(log(x)-1))/log(10))) << endl;
+    }
+}
